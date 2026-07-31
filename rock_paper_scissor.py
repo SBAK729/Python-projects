6391645f69597ddd79de32dda7b29a7574a4fb3a
SBAK729 <sench729@gmail.com>
2026-07-31 18:41:36 +0300

    refactor :  refactoring

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -14,45 +14,25 @@ def rock_paper_scissor():
     user_choice = input("Rock, Paper Or Scissor? (r/p/s): ").lower()
 
     if user_choice not in choices:
-        print("Invalid Choice!!")
+        print("Invalid Choice!! (r/p/s): ")
+        rock_paper_scissor()
 
     computer_choice =random.choice(choices)
 
-    if user_choice == "r" and computer_choice == "p":
-        print(f"Your choice: {choices_dict[user_choice]}")
-        print(f"Computer choice: {choices_dict[computer_choice]}")
-        print("You Loss!")
-
-    elif user_choice == "r" and computer_choice == "s":
-        print(f"Your choice: {choices_dict[user_choice]}")
-        print(f"Computer choice: {choices_dict[computer_choice]}")
-        print("You Win!")
-
-    elif user_choice == "s" and computer_choice == "p":
-        print(f"Your choice:{choices_dict[user_choice]}")
-        print(f"Computer choice: {choices_dict[computer_choice]}")
-        print("You Win!")
-
-    elif user_choice == "s" and computer_choice == "r":
-        print(f"Your choice:{choices_dict[user_choice]}")
-        print(f"Computer choice: {choices_dict[computer_choice]}")
-        print("You Loss!")
-
-    elif user_choice == "p" and computer_choice == "r":
-        print(f"Your choice: {choices_dict[user_choice]}")
-        print(f"Computer choice: {choices_dict[computer_choice]}")
-        print("You Win!")
-
-    elif user_choice == "p" and computer_choice == "s":
-        print(f"Your choice: {choices_dict[user_choice]}")
-        print(f"Computer choice: {choices_dict[computer_choice]}")
-        print("You Loss!")
-
-    else:
-        print(f"Your choice: {choices_dict[user_choice]}")
-        print(f"Computer choice: {choices_dict[computer_choice]}")
+    if user_choice == computer_choice :
         print("Tie")
 
+    elif (
+        (user_choice == "r" and computer_choice == "s") or 
+        (user_choice == "p" and computer_choice == "r") or 
+        (user_choice == "s" and computer_choice == "p")):
+        print("You Win!")
+
+    else:
+        print("You Lose!!")
+
+    print(f"Your choice: {choices_dict[user_choice]}")
+    print(f"Computer choice: {choices_dict[computer_choice]}")
 
     if input("Do you want to play again? (y/n): ").lower() == "y":
         rock_paper_scissor()
